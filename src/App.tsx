e7f2acf719b769240fe98600c9e8d9779cdf8b68
Jacob Clark <jacob.jh.clark@googlemail.com>
2021-03-20 23:02:38 +0000

    Increasing ? Decreasing

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,34 @@
 import React, { FunctionComponent, useState, useEffect } from 'react';
 import './App.css';
 
-const fetchTemperatureData = (setTemperature: any) => {
+const fetchTemperature = (
+  temperature: Number, 
+  setTemperature: React.Dispatch<number>, 
+  setIncreasing: React.Dispatch<String>,
+) => {
   fetch("https://cors-container.herokuapp.com/https://p1ndfl7h7h.execute-api.eu-west-1.amazonaws.com/Prod/office").then(data=> data.json()).then(tempData => {
+    tempData.temperature > temperature ? setIncreasing("🔼") : setIncreasing("🔽");
     setTemperature(tempData.temperature);
   });
 }
 
 const Temperature:FunctionComponent = () => {
-  const [temperature, setTemperature] = useState(0.0)
+  const [temperature, setTemperature] = useState<number>(0.0)
+  const [increasing, setIncreasing] = useState<String>("");
 
   useEffect(() => {
-    fetchTemperatureData(setTemperature);
+    const interval = setInterval(() => fetchTemperature(temperature, setTemperature, setIncreasing), 10000);
 
-    setInterval(() => {
-      fetchTemperatureData(setTemperature);
-    }, 10000)
-  }, [temperature])
+    return () => clearInterval(interval);
+  }, [temperature, increasing])
 
   return <>
     <div className="screen" style={{
-      background: "linear-gradient(0deg, rgba(2,0,36,1) 0%, rgba(217,5,61,1) 0%, rgba(36,185,255,1) " + ((temperature - 16) / (20 - 16)) * 100 + "%)"
+      background: "linear-gradient(0deg, rgba(2,0,36,1) 0%, rgba(217,5,61,1) 0%, rgba(36,185,255,1) " + ((temperature - 15) / (19 - 15)) * 100 + "%)"
     }}>
       <div className="centered">
-        <h1>{temperature.toFixed(2)}<sup>oC</sup></h1>
-        <h2>room: office</h2>
+        <h1>{increasing} {temperature.toFixed(2)}<sup>oC</sup></h1>
+        <h2>🏠 room: office</h2>
       </div>
     </div>
   </>
