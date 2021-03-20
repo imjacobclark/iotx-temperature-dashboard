94f385ee6fc8f41e3e970e36eaa6aa3e2f024078
Jacob Clark <jacob.jh.clark@googlemail.com>
2021-03-20 11:20:50 +0000

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { FunctionComponent, useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const fetchTemperatureData = (setTemperature: any) => {
+  fetch("https://cors-container.herokuapp.com/https://p1ndfl7h7h.execute-api.eu-west-1.amazonaws.com/Prod/office").then(data=> data.json()).then(tempData => {
+    setTemperature(tempData.temperature);
+  });
+}
+
+const Temperature:FunctionComponent = () => {
+  const [temperature, setTemperature] = useState(0.0)
+
+  useEffect(() => {
+    fetchTemperatureData(setTemperature);
+
+    setInterval(() => {
+      fetchTemperatureData(setTemperature);
+    }, 10000)
+  }, [temperature])
+
+  return <>
+    <div className="screen" style={{
+      background: "linear-gradient(0deg, rgba(2,0,36,1) 0%, rgba(217,5,61,1) 0%, rgba(36,185,255,1) " + ((temperature - 16) / (20 - 16)) * 100 + "%)"
+    }}>
+      <div className="centered">
+        <h1>{temperature.toFixed(2)}<sup>oC</sup></h1>
+        <h2>room: office</h2>
+      </div>
     </div>
-  );
+  </>
+}
+
+function App() {
+  return <Temperature />
 }
 
 export default App;
